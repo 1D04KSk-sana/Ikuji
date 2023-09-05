292a624764466c472993bc6889921f3b65518c25
1D04KSk-sana <C2105@oic.jp>
2023-09-05 15:11:33 +0900

    DBCreate

---
+++ Ikuji/Baby.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ikuji
+{
+    internal class Baby
+    {
+        public int BabyId { get; set; }
+        //オムツ・ミルク・体重・体温
+        public string BabyMain { get; set; }
+        //おしっこ・うんこ・母乳・粉ミルク
+        public string BabySub { get; set; }
+        //体重
+        public int BabyWeight { get; set; }
+        //体温
+        public double BabyTemperature { get; set; }
+        //日付
+        public int BabyDate { get; set; }
+        //時間
+        public int BabyHour { get; set; }
+        //分
+        public int BabyMinit { get; set; }
+        //コメント
+        public string BabyComment { get; set; }
+    }
+}
--- Ikuji/FormMain.Designer.cs
+++ Ikuji/FormMain.Designer.cs
@@ -110,6 +110,7 @@
             this.Name = "FormMain";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
 
         }
